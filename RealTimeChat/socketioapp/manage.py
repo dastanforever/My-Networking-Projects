7ab51e8c84ca0609dcb6897820c071ef8b89d0e7
ashwani-pandey <ashwani.pnd@gmail.com>
2015-11-12 00:03:20 +0530

    problem with static files

--- RealTimeChat/socketioapp/manage.py
+++ RealTimeChat/socketioapp/manage.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socketioapp.settings")
     patch_psycopg()
 
-	from django.core.wsgi import get_wsgi_application
-	application = get_wsgi_application()    
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()    
 
 
     from django.core.management import execute_from_command_line
--- RealTimeChat/socketioapp/socketioapp/urls.py
+++ RealTimeChat/socketioapp/socketioapp/urls.py
@@ -1,10 +1,19 @@
-from django.conf.urls import include, url
+
+from django.conf import settings
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+
+
+from socketio import sdjango
+sdjango.autodiscover()
+
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'socketioapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^',include('echo_server.urls')),
+    url(r'^socket\.io', include(sdjango.urls)),
 ]
